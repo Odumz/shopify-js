2b9e45f1f3f7f48df983c8f62d4a44568e09331e
Odumz <odumz20@gmail.com>
2021-10-16 13:49:10 +0100

    build script added

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 try {
     app.listen(PORT, () => console.log(`Server running on Port:${PORT}`));
