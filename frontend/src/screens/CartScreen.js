1649d49ffa19b4a98b5eec70a56daf8a7591ef0f
Odumz <odumz20@gmail.com>
2021-11-25 13:25:33 +0100

    port fixes

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -37,7 +37,7 @@ const CartScreen = () => {
                 <h2>Shopping Cart</h2>
                 {cartItems.length === 0 ? (
                     <div>
-                        Your cart is empty <Link to = '/'>Go Back</Link>
+                        Your cart is empty <Link to = '/'>Go Home</Link>
                     </div>
                 ) : cartItems.map(item => (
                     <CartItem 
